5f3ac03203fbddb93dab396feb50b2203c5c0afc
Lucas <cascamargo92@gmail.com>
2016-02-09 16:41:18 -0200

    geo

--- mysite/gmaps/models.py
+++ mysite/gmaps/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from geoposition import Geoposition
-from geoposition.fields import GeopositionField
+#from geoposition import Geoposition
+#from geoposition.fields import GeopositionField
 
 class PointOfInterest(models.Model):
     name = models.CharField(max_length=100)
